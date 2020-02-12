548afb6f5ce743864e87ad630ea5d47b302b5969
Javier Rodriguez garcia <javrodri@c1r3s3.42madrid.com>
2020-02-12 12:40:22 +0100

    Correct functions + test + main

---
+++ get_next_line/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javrodri <javrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/11 16:29:19 by ncolomer          #+#    #+#             */
+/*   Updated: 2020/01/15 16:40:41 by javrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int
+	main(void)
+{
+	int		r;
+	char	*line;
+
+	line = NULL;
+	while ((r = get_next_line(&line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = NULL;
+	}
+	printf("%s", line);
+	free(line);
+	line = NULL;
+}
